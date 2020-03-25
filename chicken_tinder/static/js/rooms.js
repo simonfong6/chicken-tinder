bacf2716170a305ffcc951ca3ed44157fe5ace52
Simon <simonfong6@gmail.com>
2020-03-24 18:56:45 -0700

    working left right buttons

--- chicken_tinder/static/js/rooms.js
+++ chicken_tinder/static/js/rooms.js
@@ -29,9 +29,58 @@ function updateRestaurantCards(restaurants) {
   }
 }
 
+function findSetIndex() {
+  for (let i = 0; i < restaurants_displayed.length; i++) {
+    let displayed = restaurants_displayed[i];
+
+    // Found the set one, return it.
+    if (displayed) {
+      return i;
+    }
+  }
+}
+
+
+function nextRestaurant() {
+  let restaurants = getRestaurants();
+
+  let setDisplay = false;
+  for (let i = 0; i < restaurants_displayed.length; i++) {
+    let displayed = restaurants_displayed[i];
+
+    if (setDisplay) {
+      restaurants_displayed[i] = true;
+      break;
+    }
+
+    // Found the set one, need to set the next one in array.
+    if (displayed) {
+      setDisplay = true;
+      restaurants_displayed[i] = false;
+    }
+  }
+
+  updateRestaurantCards(restaurants);
+}
+
+
+function rejectRestaurant() {
+  let setIndex = findSetIndex();
+  console.log("Reject: " + setIndex);
+  nextRestaurant();
+}
+
+
+function acceptRestaurant() {
+  let setIndex = findSetIndex();
+  console.log("Accept: " + setIndex);
+
+  nextRestaurant();
+}
+
 // Only run after the page setup.
 $( document ).ready(function() {
-  restaurants = $( ".restaurants" );
+  let restaurants = getRestaurants();
 
   initRestaurants(restaurants);
 
