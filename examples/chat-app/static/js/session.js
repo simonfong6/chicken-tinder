aeb2cf07c1ca1d24ec56fb6a3a0b691da29d462e
Simon <simonfong6@gmail.com>
2020-03-24 23:58:56 -0700

    initial frontend stuff

---
+++ examples/chat-app/static/js/session.js
@@ -0,0 +1,17 @@
+var socket = io.connect('http://' + document.domain + ':' + location.port);
+
+socket.on( 'connect', function() {
+  socket.emit( 'my event', {
+    data: 'User Connected'
+  })
+  var form = $( 'form' ).on( 'submit', function( e ) {
+    e.preventDefault()
+    let user_name = $( 'input.username' ).val()
+    let user_input = $( 'input.message' ).val()
+    socket.emit( 'my event', {
+      user_name : user_name,
+      message : user_input
+    })
+    $( 'input.message' ).val( '' ).focus()
+  })
+})
