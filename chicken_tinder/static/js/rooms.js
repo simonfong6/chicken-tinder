01ac1db1c74d0c618b0d428923964ab92feb08f3
Simon <simonfong6@gmail.com>
2020-03-24 18:41:19 -0700

    Javascript to display only one restaurant

--- chicken_tinder/static/js/rooms.js
+++ chicken_tinder/static/js/rooms.js
@@ -1,6 +1,44 @@
 // Functions.
 
+// Determines whether the restaurant is displayed or not.
+let restaurants_displayed = [];
+
+function getRestaurants() {
+  restaurants = $( ".restaurants" );
+  return restaurants;
+}
+
+
+function initRestaurants(restaurants) {
+  for (let i = 0; i < restaurants.length; i++) {
+    restaurants_displayed.push(false);
+  }
+}
+
+function updateRestaurantCards(restaurants) {
+  for (let i = 0; i < restaurants.length; i++) {
+    let restaurant = restaurants[i];
+    let display = restaurants_displayed[i];
+
+    if (display) {
+      restaurant.style.display = "block";
+    } else {
+      restaurant.style.display = "none";
+    }
+
+  }
+}
+
 // Only run after the page setup.
 $( document ).ready(function() {
-  console.log( "ready!" );
+  restaurants = $( ".restaurants" );
+
+  initRestaurants(restaurants);
+
+  updateRestaurantCards(restaurants);
+
+  restaurants_displayed[0] = true;
+
+  updateRestaurantCards(restaurants);
+
 });
