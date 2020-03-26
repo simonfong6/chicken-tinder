c2acc03cb6e93a7b089b769728b7bcf43bcf349a
Simon Fong <simonfong6@gmail.com>
2020-03-26 23:52:24 +0000

    docker stuffs

--- .devcontainer/docker_attach.sh
+++ .devcontainer/docker_attach.sh
@@ -3,9 +3,9 @@ DIRECTORY_NAME="chicken-tinder"
 docker container run \
     -it \
     --rm \
-    --name my-boba-container \
+    --name chickentinder.info.image.container \
     --user vscode \
     --mount type=bind,source=/home/ubuntu/Projects/$DIRECTORY_NAME,target=/workspace/$DIRECTORY_NAME \
     --workdir /workspace/$DIRECTORY_NAME \
     --publish 7777:7777 \
-    e3cd678e04eb /bin/bash && pip3 install -r requirments.txt
+    chickentinder.info.image /bin/bash && pip3 install -r requirments.txt
